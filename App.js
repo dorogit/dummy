9d4149d22e7ffe189de4b8e65f8a90eb9d735559
Tushar Jain <ishaanjainlock10@gmail.com>
2023-05-15 09:14:08 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HomeScreen from "./src/HomeScreen";
+import { NavigationContainer } from "react-navigation";
+
+const App = () => {
+  return (
+    <HomeScreen/>
+  )
+}
+
+export default App;
